436f31f0c3114e5d7e2c99eae1d7ea212ccc9c2c
Evan Chu <echu34@gmail.com>
2021-01-13 20:52:08 -0500

    Added questions

--- index.js
+++ index.js
@@ -1,7 +1,57 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type:"input",
+        name: "title",
+        message: "What is the title of the project?", 
+    },
+    {
+        type:"input",
+        name: "description",
+        message: "Please give a short description of the project", 
+    },
+    {
+        type:"input",
+        name: "install" ,
+        message: "Please describe the install process (if any)", 
+    },
+    {
+        type:"input",
+        name: "use",
+        message: "What is the use of the app?", 
+    },
+    {
+        type:"input",
+        name: "contribute",
+        message: "Name the people who are contributing", 
+    },
+    {
+        type:"input",
+        name: "test",
+        message: "Is there tests to be done?", 
+    },
+    {
+        type:"input",
+        name: "questions",
+        message: "Where can I get ask questions?", 
+    },
+    {
+        type:"input",
+        name: "username",
+        message: "Enter your GitHub username", 
+    },
+    {
+        type:"input",
+        name: "email",
+        message: "Please enter your email: ", 
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
